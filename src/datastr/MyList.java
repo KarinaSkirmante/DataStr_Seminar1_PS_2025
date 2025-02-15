e2ce827815fff54455f30e7ada52244f8e289677
KarinaSkirmante <karina.krinkele@gmail.com>
2025-02-15 15:27:59 +0200

    Add: added variables and contructors in MyList

--- src/datastr/MyList.java
+++ src/datastr/MyList.java
@@ -1,5 +1,25 @@
 package datastr;
 
 public class MyList {
+	//mainīgie
+	private char[] list;
+	private final int DEFAULT_SIZE = 10;
+	private int size = DEFAULT_SIZE;
+	private int counter = 0;
+	
+	//bezargumenta konstruktors
+	public MyList() {
+		list = new char[size];
+	}
+	
+	//argumenta konstruktors
+	public MyList(int inputSize) {
+		if(inputSize > 0) {
+			size = inputSize;
+		}
+		
+		list = new char[size];	
+	}
+	
 
 }
